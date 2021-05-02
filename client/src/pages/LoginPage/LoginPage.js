41e9de7fd6c504b54845cb0a2b46250945f4abce
Simkav <simkav2411@gmail.com>
2021-05-02 13:30:25 +0300

    Refactor loginPage

--- client/src/pages/LoginPage/LoginPage.js
+++ client/src/pages/LoginPage/LoginPage.js
@@ -8,32 +8,32 @@ import { clearErrorSignUpAndLogin } from '../../actions/actionCreator';
 import CONSTANTS from '../../constants';
 
 const LoginPage = (props) => {
-  const changeRoute = () => {
-    props.history.replace('/');
-  };
-  return (
-    <div className={ styles.mainContainer }>
-      <div className={ styles.loginContainer }>
-        <div className={ styles.headerSignUpPage }>
-          <Logo src={ `${ CONSTANTS.STATIC_IMAGES_PATH }logo.png` } alt="logo"/>
-          <div className={ styles.linkLoginContainer }>
-            <Link to='/registration'
-                  style={ {textDecoration: 'none'} }><span>Signup</span></Link>
-          </div>
+    const changeRoute = () => {
+        props.history.replace('/');
+    };
+    return (
+        <div className={styles.mainContainer}>
+            <div className={styles.loginContainer}>
+                <div className={styles.headerSignUpPage}>
+                    <Logo src={`${CONSTANTS.STATIC_IMAGES_PATH}logo.png`} alt="logo"/>
+                    <div className={styles.linkLoginContainer}>
+                        <Link to='/registration'
+                              className={styles.link}><span>Signup</span></Link>
+                    </div>
+                </div>
+                <div className={styles.loginFormContainer}>
+                    <LoginForm changeRoute={changeRoute}/>
+                </div>
+            </div>
         </div>
-        <div className={ styles.loginFormContainer }>
-          <LoginForm changeRoute={ changeRoute }/>
-        </div>
-      </div>
-    </div>
-  );
+    );
 
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return {
-    clearError: () => dispatch(clearErrorSignUpAndLogin()),
-  };
+    return {
+        clearError: () => dispatch(clearErrorSignUpAndLogin()),
+    };
 };
 
 export default connect(null, mapDispatchToProps)(LoginPage);
