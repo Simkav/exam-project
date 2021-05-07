9a6818e13495184d9c52f7f8c3c4576745ac4ab3
Simkav <simkav2411@gmail.com>
2021-05-07 13:05:14 +0300

    Add controller for transactionHistory

---
+++ server/src/server/controllers/transactionsController.js
@@ -0,0 +1,28 @@
+const { User, TransactionHistory,sequelize } = require('../models/index');
+const createHttpError = require('http-errors');
+
+module.exports.getTransactionsHistory = async (req, res, next) => {
+    try {
+        const { tokenData: { userId } } = req
+        const userInstance = await User.findByPk(userId)
+        const history = await userInstance.getTransactionHistories({ attributes: { exclude: ['userId'] } })
+        res.send({ data: { history } })
+    } catch (err) {
+        console.log(err)
+        next(err)
+    }
+}
+module.exports.getTotalMoneyFlow = async (req, res, next) => {
+    try {
+        const { tokenData: { userId } } = req
+        const userInstance = await User.findByPk(userId)
+        const flow = await userInstance.getTransactionHistories({
+            group: 'operationType',
+            attributes: { exclude: ['userId','id','createdAt','updatedAt','sum'], include: [[sequelize.fn('sum', sequelize.col('sum')), 'total']] }
+        })
+        res.send({data:{flow}})
+    } catch (err) {
+        console.log(err)
+        next(err)
+    }
+}
