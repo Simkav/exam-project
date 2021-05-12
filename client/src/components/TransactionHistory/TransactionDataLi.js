85a7051b3d1364a3d2be0ad8d899e1ee1766fdf8
Simkav <simkav2411@gmail.com>
2021-05-12 13:13:56 +0300

    Refactor Transaction components

--- client/src/components/TransactionHistory/TransactionDataLi.js
+++ client/src/components/TransactionHistory/TransactionDataLi.js
@@ -7,8 +7,8 @@ const TransactionDataLi = (props) => {
     return (
         <li className={cx(styles['table-row'])}>
             <div className={cx(styles.col, styles['col-1'])}>{data[0]}</div>
-            <div className={cx(styles.col, styles['col-2'])}>{data[1]}</div>
-            <div className={cx(styles.col, styles['col-3'])}>{data[2]}</div>
+            <div className={cx(styles.col, styles['col-2'])}>{data[2]}</div>
+            <div className={cx(styles.col, styles['col-3'])}>{data[1]}</div>
         </li>
     )
 }
--- client/src/components/TransactionHistory/TransactionHistory.js
+++ client/src/components/TransactionHistory/TransactionHistory.js
@@ -5,32 +5,36 @@ import cx from 'classnames'
 import styles from './TransactionHistory.module.sass'
 import TableLi from './TransactionDataLi'
 import * as ActionCreators from "../../actions/actionCreator";
+import Spinner from "../Spinner/Spinner";
 
 const TransactionHistory = () => {
     useEffect(() => {
         getTransactionsInfo()
     }, [])
     const dispatch = useDispatch()
-    const { data, error, isFetcing, totalFlow } = useSelector(({ transactions }) => transactions)
+    const { data, error, isFetching, totalFlow } = useSelector(({ transactions }) => transactions)
     const { getTransactionsInfo } = bindActionCreators(ActionCreators, dispatch)
     return (
+
         <div className={cx(styles.container)}>
-            <ul className={cx(styles['responsive-table'])}>
-                <li className={cx(styles['table-header'])}>
-                    <div className={cx(styles.col, styles['col-1'])}>Id</div>
-                    <div className={cx(styles.col, styles['col-2'])}>Type</div>
-                    <div className={cx(styles.col, styles['col-3'])}>Amount</div>
-                </li>
-                {
-                    totalFlow.map((el) => <TableLi key={el[0]} data={["TOTAL",el[0],el[1]]}/>)
-                }
-                {
-                    data.length ?
-                        data.map((el) => <TableLi key={el.id} data={Object.values(el)}/>)
-                        : <div style={{ textAlign: "center", marginBottom: "20px" }}>No history</div>
-                }
-            </ul>
+            {isFetching ? <Spinner/> :
+                <ul className={cx(styles['responsive-table'])}>
+                    <li className={cx(styles['table-header'])}>
+                        <div className={cx(styles.col, styles['col-1'])}>Id</div>
+                        <div className={cx(styles.col, styles['col-2'])}>Type</div>
+                        <div className={cx(styles.col, styles['col-3'])}>Amount</div>
+                    </li>
+                    {
+                        totalFlow.map((el) => <TableLi key={el[0]} data={["TOTAL", el[1], el[0]]}/>)
+                    }
+                    {
+                        data.length ?
+                            data.map((el) => <TableLi key={el.id} data={Object.values(el)}/>)
+                            : <div style={{ textAlign: "center", marginBottom: "20px" }}>No history</div>
+                    }
+                </ul>}
         </div>
+
     )
 }
 export default TransactionHistory
